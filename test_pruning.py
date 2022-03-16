595d4e93bdb9647556b9d2ff3f11ebcfbbadc546
Victor Torres <v.torres621@gmail.com>
2022-03-16 15:51:12 -0700

    add pruning test example

--- test_pruning.py
+++ test_pruning.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.utils.prune as prune
 from torch.utils.data import DataLoader
 from src.extract_dataset import extract_dataset
 from src.dataset import MNIST_dataset
@@ -26,6 +27,57 @@ model = CNN()
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.eval()
 model.to(device)
+
+module = model.conv1
+#print(list(module.named_parameters()))
+#print(list(module.named_buffers()))
+
+parameters_to_prune = (
+    (model.conv1, 'weight'),
+    (model.conv2, 'weight'),
+    (model.fc1, 'weight'),
+)
+
+prune.global_unstructured(
+    parameters_to_prune,
+    pruning_method=prune.L1Unstructured,
+    amount=0.5,
+)
+
+
+print(
+    "Sparsity in conv1.weight: {:.2f}%".format(
+        100. * float(torch.sum(model.conv1.weight == 0))
+        / float(model.conv1.weight.nelement())
+    )
+)
+print(
+    "Sparsity in conv2.weight: {:.2f}%".format(
+        100. * float(torch.sum(model.conv2.weight == 0))
+        / float(model.conv2.weight.nelement())
+    )
+)
+print(
+    "Sparsity in fc1.weight: {:.2f}%".format(
+        100. * float(torch.sum(model.fc1.weight == 0))
+        / float(model.fc1.weight.nelement())
+    )
+)
+
+print(
+    "Global sparsity: {:.2f}%".format(
+        100. * float(
+            torch.sum(model.conv1.weight == 0)
+            + torch.sum(model.conv2.weight == 0)
+            + torch.sum(model.fc1.weight == 0)
+        )
+        / float(
+            model.conv1.weight.nelement()
+            + model.conv2.weight.nelement()
+            + model.fc1.weight.nelement()
+        )
+    )
+)
 
 #Test loop
 print(f"[INFO] Performing Inference on {len(test_set)} examples...")
